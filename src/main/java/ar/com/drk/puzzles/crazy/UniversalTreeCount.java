08601d3b33c230395b669504fe41ce4e08c20f2f
Leandro Fernandez <leandro_hernan.fernandez@roche.com>
2020-01-31 16:07:07 -0300

    Improve

--- src/main/java/ar/com/drk/puzzles/crazy/UniversalTreeCount.java
+++ src/main/java/ar/com/drk/puzzles/crazy/UniversalTreeCount.java
@@ -5,46 +5,66 @@ import java.util.stream.IntStream;
 
 public class UniversalTreeCount {
 
-    static class Node {
-        final int value;
+    static class Node<T> {
+        final T value;
         final Node left;
         final Node right;
-        public Node(final int value, final Node left, final Node right) {
+        public Node(final T value, final Node left, final Node right) {
             this.value = value;
             this.left = left;
             this.right = right;
         }
     }
 
+    static class Response {
+        int count;
+        boolean isUnival;
+        public Response(final int count, final boolean isUnival) {
+            this.count = count;
+            this.isUnival = isUnival;
+        }
+    }
+
     private static int count = 0;
 
     public static void main(String[] args) {
-        int values[] = new int[] {1,0,1,0,1,1,1,1,1,1,1,1,1};
+        Integer values[] = new Integer[] {1,0,1,0,1,1,1,1,1,1,1,1,1};
+//        Character values[] = new Character[] {
+//            'a',
+//            'a', 'a',
+//            null, null, 'a', 'a',
+//            null, null, null, null, null, null, null, 'A'
+//        };
 
-        final Node root = newTreeLevelOrder(values, 0);
+        final Node<Integer> root = newTreeLevelOrder(values, 0);
 
         printTree(root);
         System.out.println("Total nodes: " + values.length);
-        System.out.println("Universal trees: " + countUniversalTrees(root));
+        System.out.println("Universal trees: " + countUniversalTrees(root).count);
         System.out.println("countUniversalTrees() hits: " + count);
     }
 
-    private static int countUniversalTrees(Node node) {
+    private static <T> Response countUniversalTrees(Node<T> node) {
         count++;
         if (node.left == null && node.right == null)
-            return 1;
-        if (node.left == null)
-            return countUniversalTrees(node.right)
-                    + ((node.value == node.right.value) ? 1 : 0);
-        if (node.right == null)
-            return countUniversalTrees(node.left)
-                    + ((node.value == node.left.value) ? 1 : 0);
-        return countUniversalTrees(node.left)
-                + countUniversalTrees(node.right)
-                + ((node.value == node.left.value && node.value == node.right.value) ? 1 : 0);
+            return new Response(1, true);
+        if (node.left == null){
+            final Response response = countUniversalTrees(node.right);
+            final boolean amIUnival = node.value.equals(node.right.value) && response.isUnival;
+            return new Response(response.count + (amIUnival ? 1 : 0), amIUnival);
+        }
+        if (node.right == null) {
+            final Response response = countUniversalTrees(node.left);
+            final boolean amIUnival = node.value.equals(node.left.value) && response.isUnival;
+            return new Response(response.count + (amIUnival ? 1 : 0), amIUnival);
+        }
+        final Response responseLeft = countUniversalTrees(node.left);
+        final Response responseRight = countUniversalTrees(node.right);
+        final boolean amIUnival = node.value.equals(node.left.value) && node.value.equals(node.right.value) && responseLeft.isUnival && responseRight.isUnival;
+        return new Response(responseLeft.count + responseRight.count + (amIUnival ? 1 : 0), amIUnival);
     }
 
-    private static void printTree(final Node node) {
+    private static <T> void printTree(final Node<T> node) {
         BitSet levelStatus = new BitSet();
         printTree(node, 0, levelStatus);
     }
@@ -65,9 +85,11 @@ public class UniversalTreeCount {
         levelStatus.set(level);
     }
 
-    private static Node newTreeLevelOrder(final int[] values, final int index) {
+    private static <T> Node<T> newTreeLevelOrder(final T[] values, final int index) {
         if (index < values.length) {
-            return new Node(
+            if (values[index] == null)
+                return null;
+            return new Node<>(
                     values[index],
                     newTreeLevelOrder(values, 2 * index + 1),
                     newTreeLevelOrder(values, 2 * index + 2)
