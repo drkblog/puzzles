e20a0b49937b5718f2fc6d4270a7abfb382ee91d
Leandro <drkbugs@gmail.com>
2020-02-01 22:48:28 -0300

    Steps num ways and fixed print tree for UniversalTreeCount

---
+++ src/main/java/ar/com/drk/puzzles/crazy/FrogRiverOne.java
@@ -0,0 +1,19 @@
+package ar.com.drk.puzzles.crazy;
+
+import java.util.BitSet;
+
+public class FrogRiverOne {
+    public int solution(int X, int[] A) {
+        BitSet places = new BitSet();
+        int placesCovered = 0;
+        int second = 0;
+        while(placesCovered < X && second < A.length) {
+            if (!places.get(A[second])) {
+                places.set(A[second]);
+                placesCovered++;
+            }
+            second++;
+        }
+        return (placesCovered < X) ? -1 : second-1;
+    }
+}
