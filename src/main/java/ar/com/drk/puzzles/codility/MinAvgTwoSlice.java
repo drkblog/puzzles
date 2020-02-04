85f04f81afbd700e11a07e4741779c41a5010da6
Leandro <drkbugs@gmail.com>
2020-02-04 01:29:30 -0300

    MinAvgTwoSlice

---
+++ src/main/java/ar/com/drk/puzzles/codility/MinAvgTwoSlice.java
@@ -0,0 +1,28 @@
+package ar.com.drk.puzzles.codility;
+
+public class MinAvgTwoSlice {
+
+    public static void main(String[] args) {
+        int[] t1 = new int[] {4, 2, 2, 5, 1, 5, 8}; // 1
+        int[] t2 = new int[] {-3, -5, -8, -4, -10}; // 2
+        System.out.println("Result: " + solution(t2));
+    }
+
+    static int solution(int[] A) {
+        double[] average = new double[A.length - 1];
+        for (int i=0; i<A.length-1; i++) {
+            average[i] = ((double) A[i] + A[i+1]) / 2;
+        }
+        double min = 10000;
+        int p=0;
+        int firstMinimum = 0;
+        while(p < A.length-1) {
+            if (average[p] < min) {
+                min = average[p];
+                firstMinimum = p;
+            }
+            p++;
+        }
+        return firstMinimum;
+    }
+}
