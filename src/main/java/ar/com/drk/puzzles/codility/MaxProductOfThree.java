c6bf12eaf758e5437fe7edd8d602224778608a15
Leandro <drkbugs@gmail.com>
2020-02-13 01:03:38 -0300

    Broken MaxProductOfThree.java

---
+++ src/main/java/ar/com/drk/puzzles/codility/MaxProductOfThree.java
@@ -0,0 +1,37 @@
+package ar.com.drk.puzzles.codility;
+
+public class MaxProductOfThree {
+    public static void main(String[] args) {
+        int[] A = new int[]{-3, 1, 2, -2, 5, 6};
+        int[] B = new int[]{-5, 5, -5, 4};
+        System.out.println("Max triplet is: " + solution(B));
+    }
+
+    static int solution(int[] A) {
+        return heap[0] * heap[1] * heap[2];
+    }
+
+    static class Heap {
+        private final int[] heap;
+
+
+        public Heap(int size) {
+            heap = new int[size];
+        }
+
+        public int[] findHighestValues(int[] values) {
+
+        }
+
+        private int findMinimum() {
+            int minimum = 0;
+            for (int i = 0; i < heap.length; i++) {
+                if (i > 0 && heap[i] < heap[i - 1]) {
+                    minimum = i;
+                }
+            }
+            return minimum;
+        }
+    }
+
+}
