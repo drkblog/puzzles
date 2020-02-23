0787bf97b8917a9ceb7555939ca6cf110657d1b4
Leandro <drkbugs@gmail.com>
2020-02-22 21:52:01 -0300

    NumberOfDiscIntersections n^2

--- src/main/java/ar/com/drk/puzzles/codility/NumberOfDiscIntersections.java
+++ src/main/java/ar/com/drk/puzzles/codility/NumberOfDiscIntersections.java
@@ -1,18 +1,21 @@
 package ar.com.drk.puzzles.codility;
 
 public class NumberOfDiscIntersections {
+
+    // N^2 https://app.codility.com/demo/results/trainingC5R4UX-ZVR/
+
     public static void main(String[] args) {
         final int[] test = new int[]{1, 5, 2, 1, 4, 0};
-        System.out.println("Intersections: " + solution(test));
+        final int[] testOverflow = new int[]{2147483647, 2147483647};
+        System.out.println("Intersections: " + solution(testOverflow));
     }
 
     static int solution(int[] A) {
         int intersections = 0;
         for (int i = 0; i < A.length - 1; i++) {
             for (int j = i + 1; j < A.length; j++) {
-                if (A[i] + A[j] - (j - i) >= 0) {
+                if ((long) A[i] + A[j] - (j - i) >= 0) {
                     intersections++;
-                    System.out.println("[" + i + "," + j + "]");
                 }
             }
         }
