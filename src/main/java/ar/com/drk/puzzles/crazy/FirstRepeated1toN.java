414359577c6f10b3fab5b32350b469dae3da716c
Leandro <drkbugs@gmail.com>
2020-03-01 23:21:42 -0300

    CircularRotation sucks, haha

---
+++ src/main/java/ar/com/drk/puzzles/crazy/FirstRepeated1toN.java
@@ -0,0 +1,22 @@
+package ar.com.drk.puzzles.crazy;
+
+import java.util.Arrays;
+
+public class FirstRepeated1toN {
+    public static void main(String[] args) {
+        final int[] test = new int[]{2, 1, 3, 5, 3, 4, 2};
+        System.out.println("First repeated in " + Arrays.toString(test) + " = " + solution(test));
+    }
+
+    private static int solution(int[] A) {
+        int offset = A.length + 1;
+        for (int i = 0; i < A.length; i++) {
+            int pointer = A[i];
+            if (pointer > offset) {
+                return pointer - offset;
+            }
+            A[pointer] += offset;
+        }
+        return -1;
+    }
+}
