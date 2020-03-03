5980417208e2d2a0bdbb52ccbd385af147f6f5a0
Leandro Fernandez <leandro_hernan.fernandez@roche.com>
2020-03-03 10:00:59 -0300

    AlmostPalindrome.java

--- src/main/java/ar/com/drk/puzzles/crazy/CircularRotation.java
+++ src/main/java/ar/com/drk/puzzles/crazy/CircularRotation.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class CircularRotation {
     public static void main(String[] args) {
-        int[] test = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int[] test = new int[]{1, 2, 3, 4, 5, 6};
         int[] testL = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
         for (int k = 1; k <= test.length; k++) {
             System.out.println("k[" + k + "]" + Arrays.toString(solution(test, k)));
@@ -14,21 +14,26 @@ public class CircularRotation {
     private static int[] solution(final int[] I, int k) {
         int[] A = Arrays.copyOf(I, I.length); //  Just for testing
         for (int i = 0; i < A.length - k; i++) {
-            swap(A, i, i + k, k);
+            swapWithOverflow(A, i, i + k, k);
         }
-//        if (A.length % 2 > 0) {
-//            for (int i = 0; i < k - 1; i++) {
-//                swap(A, i, i + 1, k);
+//        if (A.length / 2 > 0) {
+//            if (k < A.length / 2) {
+//                swap(A, 0, A.length - 1);
 //            }
 //        }
         return A;
     }
 
-    private static void swap(int[] A, int s, int d, int overflow) {
+    private static void swapWithOverflow(int[] A, int s, int d, int overflow) {
         int x = s % overflow;
-        A[x] = A[x] ^ A[d];
-        A[d] = A[d] ^ A[x];
-        A[x] = A[x] ^ A[d];
+        int y = (d < A.length) ? d : d - (A.length);
+        swap(A, x, y);
+    }
+
+    private static void swap(int[] A, int x, int y) {
+        A[x] = A[x] ^ A[y];
+        A[y] = A[y] ^ A[x];
+        A[x] = A[x] ^ A[y];
     }
 
 }
