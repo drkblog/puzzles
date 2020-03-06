32e7f4049298818c33ad69fd159f52ca228edca9
Leandro <drkbugs@gmail.com>
2020-03-06 00:54:53 -0300

    MaxProductOfThree heap half way

--- src/main/java/ar/com/drk/puzzles/codility/MaxProductOfThree.java
+++ src/main/java/ar/com/drk/puzzles/codility/MaxProductOfThree.java
@@ -8,26 +8,48 @@ public class MaxProductOfThree {
     }
 
     static int solution(int[] A) {
+        Heap maxHeap = new Heap(3);
+        for (int i = 0; i < A.length; i++) {
+            maxHeap.accept(A[i]);
+        }
         return 1;
     }
 
     static class Heap {
+        private final int size;
         private final int[] heap;
 
+        private int minimumPosition = 0;
+        private int count = 0;
 
         public Heap(int size) {
+            this.size = size;
             heap = new int[size];
+            for (int i = 0; i < heap.length; i++) {
+                heap[i] = Integer.MIN_VALUE;
+            }
         }
 
+        public void accept(int value) {
+            if (count < size) {
+                heap[count] = value;
+                count++;
+            } else if (value > heap[minimumPosition]) {
+                heap[minimumPosition] = value;
+            }
+            minimumPosition = findMinimumPosition();
+        }
 
-        private int findMinimum() {
-            int minimum = 0;
-            for (int i = 0; i < heap.length; i++) {
-                if (i > 0 && heap[i] < heap[i - 1]) {
-                    minimum = i;
+        private int findMinimumPosition() {
+            int minimum = Integer.MAX_VALUE;
+            int minimumPosition = 0;
+            for (int i = 0; i < count; i++) {
+                if (heap[i] < minimum) {
+                    minimum = heap[i];
+                    minimumPosition = i;
                 }
             }
-            return minimum;
+            return minimumPosition;
         }
     }
 
