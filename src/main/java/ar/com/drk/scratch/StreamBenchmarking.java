bac61a42f86a90d3a955bf0dd6c97c0a09fba7c0
Leandro <drkbugs@gmail.com>
2021-04-05 01:42:04 -0300

    Actual benchmark

--- src/main/java/ar/com/drk/scratch/StreamBenchmarking.java
+++ src/main/java/ar/com/drk/scratch/StreamBenchmarking.java
@@ -1,38 +1,40 @@
 package ar.com.drk.scratch;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Sets;
+import com.google.common.collect.Lists;
 
-import java.util.Set;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class StreamBenchmarking {
 
-  private final Set<Integer> numbers = Sets.newHashSet();
-
-  private void add(final int number) {
-    numbers.add(number);
-  }
-
-  private boolean isEven(final int number) {
-    return number % 2 == 0;
-  }
-
-  private String convert(final int number) {
-    numbers.remove(number);
-    return Integer.toString(number);
-  }
-
-  public void run() {
-    IntStream.range(0, 1000000)
-        .parallel()
-        .peek(this::add)
-        .filter(this::isEven)
-        .mapToObj(this::convert)
-        .collect(ImmutableList.toImmutableList());
+  private static void benchmark(final String name, final Runnable runnable, final int times) {
+    final Instant start = Instant.now();
+    for (int i = 0; i < times; i++) {
+      runnable.run();
+    }
+    final Instant end = Instant.now();
+    System.out.println(name + " - Execution took " + Duration.between(start, end).toMillis() + "ms");
   }
 
   public static void main(final String[] args) throws Throwable {
-    new StreamBenchmarking().run();
+    final List<String> words = IntStream.range(0, 10000)
+        .mapToObj(i -> String.format("%04d", i))
+        .collect(Collectors.toList());
+
+    benchmark("stream", () -> {
+      final List<Integer> integers = words.stream()
+          .map(Integer::valueOf)
+          .collect(Collectors.toList());
+    }, 5000);
+
+    benchmark("for", () -> {
+      final List<Integer> integers = Lists.newArrayList();
+      for (int i = 0; i < words.size(); i++) {
+        integers.add(Integer.valueOf(words.get(i)));
+      }
+    }, 5000);
   }
 }
