c09eb0174708e2f39602365aeb8d549ec0a9eae6
fernal27 <leandro_hernan.fernandez@roche.com>
2021-06-01 01:37:53 -0300

    Fixes

--- src/main/java/ar/com/drk/puzzles/crazy/FirstRepeated1toN.java
+++ src/main/java/ar/com/drk/puzzles/crazy/FirstRepeated1toN.java
@@ -3,17 +3,28 @@ package ar.com.drk.puzzles.crazy;
 import java.util.Arrays;
 
 public class FirstRepeated1toN {
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     final int[] test = new int[]{2, 1, 3, 5, 3, 4, 2};
-    System.out.println("First repeated in " + Arrays.toString(test) + " = " + solution(test));
+    System.out.println("First repeated in " + Arrays.toString(test) + " = " + solution2(test));
   }
 
-  private static int solution(int[] A) {
-    int offset = A.length + 1;
+  private static int solution1(final int[] A) {
+    final boolean[] marker = new boolean[A.length];
     for (int i = 0; i < A.length; i++) {
-      int pointer = A[i];
-      if (pointer > offset) {
-        return pointer - offset;
+      if (marker[A[i]]) {
+        return A[i];
+      }
+      marker[A[i]] = true;
+    }
+    return -1;
+  }
+
+  private static int solution2(final int[] A) {
+    final int offset = A.length + 1;
+    for (int i = 0; i < A.length; i++) {
+      final int pointer = A[i] > offset ? A[i] - offset : A[i];
+      if (A[pointer] > offset) {
+        return pointer;
       }
       A[pointer] += offset;
     }
